1a111d9aea6dcb3100d376d7d18d5518f1891484
irina <irina.mukha.01@privatbank.ua>
2021-10-15 18:06:14 +0300

    init commit

--- src/com/pb/polyanytsa/hw2/Calculator.java
+++ src/com/pb/polyanytsa/hw2/Calculator.java
@@ -1,4 +1,29 @@
-package com.pb.polyanytsa.hw2;
-
+import java.util.Scanner;
 public class Calculator {
+    public static void main(String[] args) {
+        double operand1 ;
+        double operand2 ;
+        double otvet;
+        char znak;
+        Scanner reader = new Scanner(System.in);
+        System.out.print("введите 2 числа для расчетов: ");
+        operand1  = reader.nextDouble();
+        operand2  = reader.nextDouble();
+        System.out.print("\nВведите арифметический знак (+, -, *, /): ");
+        znak = reader.next().charAt(0);
+        switch(znak) {
+            case '+': otvet = operand1  + operand2 ;
+                break;
+            case '-': otvet = operand1  - operand2 ;
+                break;
+            case '*': otvet = operand1  * operand2 ;
+                break;
+            case '/': otvet = operand1  / operand2 ;
+                break;
+            default:  System.out.printf("Ошибка");
+                return;
+        }
+        System.out.print("\nРезультат вычисления:\n");
+        System.out.printf(operand1  + " " + znak + " " + operand2  + " = " + otvet);
+    }
 }
