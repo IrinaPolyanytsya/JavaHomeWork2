f9b6bbfc7e4b8ed1a36efdb5501f19ffffeaf5ed
irina <irina.mukha.01@privatbank.ua>
2021-10-24 13:23:36 +0300

    init commit

---
+++ src/com/pb/polyanytsa/hw3/Bingo.java
@@ -0,0 +1,35 @@
+
+import java.util.Random;
+import java.util.Scanner;
+public class Bingo {
+
+    public static void main(String[] args) {
+        System.out.println("Угадай число от 0 до 100.");
+        Random random = new Random();
+        int x = random.nextInt(101);
+        int number, counter = 0;
+        int a = 0;
+        do {
+            counter++;
+            Scanner in = new Scanner(System.in);
+            System.out.print("Введите ваше число: ");
+            number = in.nextInt();
+            if (number > x)
+                System.out.println("Моё число меньше.");
+            else if (number < x) System.out.println("Моё число больше.");
+            else System.out.println("Вы угадали!");
+            if (number != x)
+            {System.out.print("Хочешь выйти - введи 1, продолжить - введи любую цифру кроме 1: ");
+            a = in.nextInt();}
+        }
+        while (number != x && a != 1);
+        System.out.println("Количество попыток: " + counter);
+        System.out.println("Конец. Верное число: " + x);
+
+    }
+}
+
+
+
+
+
