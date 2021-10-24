4507a3f7f75e4f620ac7b0f8a9e28b3a9801c22d
irina <irina.mukha.01@privatbank.ua>
2021-10-24 14:17:49 +0300

    init commit

--- src/com/pb/polyanytsa/hw3/Array.java
+++ src/com/pb/polyanytsa/hw3/Array.java
@@ -1,19 +1,57 @@
 package com.pb.polyanytsa.hw3;
+
 import java.util.Scanner;
-class GuessANumber {
+
+// 2. Программа должна позволить пользователю ввести одномерный массив
+// целых чисел размерностью 10 элементов.
+//Вывести на экран введенный массив.
+//Подсчитать сумму всех элементов массива и вывести ее на экран.
+//Подсчитать и вывести на экран количество положительных элементов.
+//Произвести сортировку этого массива от меньшего к большему по алгоритму
+// сортировки пузырьком. (https://en.wikipedia.org/wiki/Bubble_sort)
+//Вывести на экран отсортированный массив.
+public class Array {
+    // Массивы (одномерный массив).
+
     public static void main(String[] args) {
-        int UnknownNumber, UserNumber, TrysCount = 0;
-        Scanner input = new Scanner(System.in);
-        System.out.println("ЗАДАЧА: Угадать число (от 0 до 100).");
-        UnknownNumber = (int)Math.floor(Math.random() * 100);
-        do {
-            TrysCount++;
-            System.out.print("Введите ваше число: ");
-            UserNumber = input.nextInt();
-            if (UserNumber > UnknownNumber)
-                System.out.println("Моё число меньше.");
-            else if (UserNumber < UnknownNumber) System.out.println("Моё число больше.");	else System.out.println("Вы угадали!");
-        } while (UserNumber != UnknownNumber);
-        System.out.println("Количество попыток: " + TrysCount);
+        int[] array = new int[10];
+        int sum=0;
+        int pol=0;
+        Scanner in = new Scanner(System.in);
+        // Заполнение массива.
+        for (int i = 0; i < 10; i++) {
+            System.out.println("введите число: " );
+            array[i] = in.nextInt();
+            sum=sum+array[i];
+            if (array[i]>0)
+                pol=pol+1;
+        }
+        System.out.println("Ваши числа: " );
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+
+        }
+        System.out.println("Сумма ваших элементов: " +sum);
+        System.out.println("Количество положительных элементов: " +pol);
+        //
+        boolean isSorted = false;
+        int buf;
+        while(!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length-1; i++) {
+                if(array[i] > array[i+1]){
+                    isSorted = false;
+
+                    buf = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = buf;
+                }
+            }
+        }
+        System.out.println("Отсартированый массив: " );
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+
+        }
     }
 }
