76a7468aaac410f8dae72afcc0f77f54c812d5d0
irina <irina.mukha.01@privatbank.ua>
2021-10-29 13:55:12 +0300

    init commit

---
+++ src/com/pb/polyanytsa/hw4/Anagram.java
@@ -0,0 +1,14 @@
+package com.pb.polyanytsa.hw4;
+
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String[] args) {
+    Scanner str = new Scanner(System.in);
+    System.out.println ("Введи анограму 1: ");
+
+    System.out.print("Введи анограму 2: ");
+
+
+}
+}
