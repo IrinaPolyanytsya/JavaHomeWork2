013d0d40720d32ce161d9b7bbd88601607028cd0
irina <irina.mukha.01@privatbank.ua>
2021-11-01 19:48:25 +0200

    init commit

--- src/com/pb/polyanytsa/hw5/Library.java
+++ src/com/pb/polyanytsa/hw5/Library.java
@@ -1,4 +1,31 @@
 package com.pb.polyanytsa.hw5;
 
 public class Library {
+    public static void main(String[] args) {
+        Book book1 = new Book("Физика", " М. В. Ломоносов", "1986");
+        Book book2 = new Book("Химия.Основы", "Д. И. Менделеев", "1991");
+        Book book3 = new Book("Философия", " Фома Аквинский", "1270");
+        System.out.println(book1.getInfo());
+        System.out.println(book2.getInfo());
+        System.out.println(book3.getInfo());
+        Reader reader1 = new Reader("Лукяненко С.Ю.", "12255", "Философия", "03.12.1988", "+380663018530");
+        Reader reader2 = new Reader("Петров О.С.", "25874", "Химия", "05.07.1992", "+380661254878");
+        Reader reader3 = new Reader("Поляница И.И.", "28554", "ИФ", "05.04.2005", "+38095554088");
+        System.out.println(reader1.getInfo());
+        System.out.println(reader2.getInfo());
+        System.out.println(reader3.getInfo());
+
+        reader1.takeBook(5);
+        reader1.returnBook(3);
+
+        String[] takeStrBooks = {"Химия", "Философия", "Физика"};
+        String[] returnStrBooks = {"Философия", "Физика"};
+        reader2.takeBook(takeStrBooks);
+        reader2.returnBook(returnStrBooks);
+
+        Book[] takeBooks = {book1, book2, book3};
+        Book[] returnBooks = {book1, book2, book3};
+        reader3.takeBook(takeBooks);
+        reader3.returnBook(returnBooks);
+    }
 }
--- src/com/pb/polyanytsa/hw5/Reader.java
+++ src/com/pb/polyanytsa/hw5/Reader.java
@@ -1,4 +1,89 @@
 package com.pb.polyanytsa.hw5;
 
-public class Reader {
+class Reader {
+    private String fio;
+    private String readerNumber;
+    private String faculty;
+    private String birth;
+    private String phone;
+
+    public Reader(String fio, String readerNumber, String faculty, String birth, String phone) {
+        this.fio = fio;
+        this.readerNumber = readerNumber;
+        this.faculty = faculty;
+        this.birth = birth;
+        this.phone = phone;
+    }
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public String getReaderNumber() {
+        return readerNumber;
+    }
+
+    public void setReaderNumber(String readerNumber) {
+        this.readerNumber = readerNumber;
+    }
+
+    public String getFaculty() {
+        return faculty;
+    }
+
+    public void setFaculty(String faculty) {
+        this.faculty = faculty;
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void takeBook(int count) {
+        System.out.println(this.fio + " взял " + count + " книги.");
+    }
+
+    public void takeBook(String[] books) {
+        System.out.println(this.fio + " взял книги: " + String.join(", ", books));
+    }
+
+    public void takeBook(Book[] books) {
+        String[] output = new String[books.length];
+        for (int i = 0; i < books.length; i++)
+            output[i] = books[i].getName() + "(" + books[i].getAuthor() + " " + books[i].getYear() + " г.)";
+        System.out.println(this.fio + " взял книги: "+ String.join(", ", output));
+    }
+
+    public void returnBook(int number) {
+        System.out.println(this.fio + " вернул " + number + " книги.");
+    }
+    public void returnBook(String[] books) {
+        System.out.println(this.fio + " вернул книги: " + String.join(", ", books));
+    }
+
+    public void returnBook(Book[] books) {
+        String[] output = new String[books.length];
+        for (int i = 0; i < books.length; i++)
+            output[i] = books[i].getName() + "(" + books[i].getAuthor() + " " + books[i].getYear() + " г.)";
+        System.out.println(this.fio + " вернул книги: "+ String.join(", ", output));
+    }
+
+    public String getInfo() {
+        return "ФИО: " + fio + ", номер читательского билета: " + readerNumber + ", факультет: " + faculty + ", дата рождения: " + birth + ", телефон: " + phone + ".";
+    }
 }
